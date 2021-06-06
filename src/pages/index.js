eb317200724fa55885cf44e685c48d4e925b4f01
j-94 <jasedgwic@gmail.com>
2021-06-06 16:54:09 +0100

    hello world

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,5 @@
 import React from "react"
-import SEO from "../components/seo"
-import Layout from '../containers/layout/layout'
-import Header from '../containers/layout/header/header-four'
-import Footer from '../containers/layout/footer/footer-two'
-import Hero from '../containers/landing/hero'
 
-
-const IndexPage = ({location}) => (
-  <Layout location={location}>
-    <SEO/>
-    <Header transparent/>  
-    <main className="site-wrapper-reveal">
-      <Hero/>
-      
-      
-    </main>
-    <Footer/>
-  </Layout>
-)
- 
-export default IndexPage
+export default function Home() {
+  return <div>Hello world!</div>
+}
